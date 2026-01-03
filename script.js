ead41ee25576a5343dda82532ad471167f0bcb96
Shahar Hershtig <ajr2955@gmail.com>
2026-01-03 16:02:46 +0200

    secound commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the button element
+const button = document.getElementById('myButton');
+
+// Add click event listener
+button.addEventListener('click', function() {
+    console.log('Button clicked!');
+    alert('Hello! The button was clicked.');
+});
